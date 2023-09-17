6304d0b1c20533a2d68afd623c999e9aef2d7041
miletto <evandro.miletto@poa.ifrs.edu.br>
2023-09-17 19:03:53 -0300

    nova versao CV
    alteraçao html e foco para a disciplina cpw1

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/*  script para controlar o comportamento do menu  */
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+/*  FIM script para controlar o comportamento do menu  */
